d7948c3901b1113e1bf62e74ac227f6e68503971
Vadim Palatov <Vatrandero@gmail.com>
2025-01-03 19:52:00 +0300

    cfg: revised.

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,27 +1,35 @@
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    db_cfg: db_pg_cfg,
-    net_cfg: net_cfg,
+    pub db_cfg: db_cfg,
+    pub net_cfg: net_cfg,
+}
+
+#[derive(Debug,Deserialize,Serialize)]
+pub enum db_cfg{
+    Postgresql(db_pg_cfg),    
 }
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename = "db.posrgresql")]
-struct db_pg_cfg {
-    postgres_url: String,
+#[serde(rename="pg")]
+pub struct db_pg_cfg {
+    url: String,
+    users_table: String,
     use_tls: bool,
+
 }
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename = "net")]
-struct net_cfg {
-    addreses: Vec<String>,
-    port: usize,
+pub struct net_cfg {
+    pub addreses: Vec<String>,
+    pub port: usize,
 }
 impl Default for Config {
     fn default() -> Self {
-        let db_pg = db_pg_cfg {
-            postgres_url: "postgresql://user:password@localhost/dbname".to_string(),
-            use_tls: false,
-        };
+        let db_pg = db_cfg::Postgresql( db_pg_cfg {
+            url: "postgresql://user:password@localhost/dbname".to_string(),
+            users_table: "users".to_string(),
+            use_tls: false
+        });
         let ncfg = net_cfg {
             addreses: vec![String::from("127.0.0.1")],
             port: 8001,
