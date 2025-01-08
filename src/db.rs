1249ef462b50e7edc92e4556f4d0bfb93b83e898
Vadim Palatov <Vatrandero@gmail.com>
2025-01-08 16:36:05 +0300

    db: User and Session managment interface

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,22 @@
-use crate::cfg::db_pg_cfg;
-use sqlx::{Pool, Postgres}; 
+use std::fmt::Display;
 
-pub trait SQLDBUserManager {} { 
-    pub fn create_user(&self) ->  Result<(), sqlx::Error>;
-        
+use crate::cfg::db_pg_cfg;
+use crate::users::*;
+use sqlx::{Error as sqlError, Pool, Postgres};
+use uuid::Uuid;
+pub trait UsersAndSessionManager {
+    type Error: Display + std::error::Error; 
+
+    async fn register_new_user(&self, u: &User) -> Result<(), Self::Error>;
+    async fn get_user_by_id(&self, id: u64 ) -> Result<User, Self::Error >;
+    async fn register_new_session(
+        &self,
+        u: &User,
+        uuid: Option<Uuid>,
+    ) -> Result<Option<Uuid>, Self::Error>;
+    async fn resolve_user_session(&self, uid: Uuid) 
+    -> Result<Option<Uuid>, Self::Error>;
+
 }
 
-
+struct DBPostgres {}
