08f014f81e53442043a8687862d4c910f3a0fa2b
Vadim Palatov <Vatrandero@gmail.com>
2025-03-12 15:17:04 +0300

    api: wip

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,21 +1,37 @@
 /// REST Api.
 use crate::models::dtos;
-pub mod routes;
+pub(super) mod routes;
 
+use crate::db::{SessionManager, UsersManager};
 use crate::models::users::User;
-use crate::db::{UsersManager, SessionManager};
 use std::sync::Arc;
-use tokio::net::*;
+use axum::{Router, routing::{}};
+#[cfg(feature = "apidoc")]
 use utoipa::openapi::OpenApi;
 
+#[derive(Clone)]
+pub struct AppState {
+    pub dbpool_user_manager: Arc<dyn UsersManager + Send + Sync>,
+    pub dbpool_session_manager: Arc<dyn SessionManager + Send + Sync>,
+    /* Why using dyn?
+    In future, there considired multi-db mode or
+    db+cache (like redis) modes.
+    If only db provided - both Arcs needs to
+    be directedd at the same dbpool.
+    This text shall be removed when feature will
+    be implemented.
+     */
+}
 
-pub fn init(cfg: crate::cfg::Config) {}
+/// Based on passed configs 
+pub fn init_router(cfg: &crate::cfg::Config) -> axum::Router<AppState> {
+    let mut r: Router<AppState> = Router::new();
 
-pub fn generate_api_spec() -> OpenApi {
+
     todo!()
 }
 
-pub(super) struct DataPool {
-    pub dbpool_user_manager: Arc<dyn  UsersManager>, 
-    pub dbpool_session_manager: Arc<dyn SessionManager>                                                   
+#[cfg(feature="apidoc")]
+pub fn route_openapi() -> Router {
+    todo!()
 }
--- src/api/routes.rs
+++ src/api/routes.rs
@@ -1,33 +1,80 @@
-use super::dtos;
-
 pub(super) mod commons {
+    pub use super::super::AppState;
+    pub use crate::models;
+    pub use axum::extract::{State, Query};
+    pub use axum::http::{header::SET_COOKIE, StatusCode};
+    pub use axum::response::{AppendHeaders, ErrorResponse, IntoResponse};
     pub use axum::routing::{get, post, put};
-    pub use axum::http::header::{SET_COOKIE};
     pub use axum::{Json, Router};
+    pub use axum_extra::extract::CookieJar;
+    pub use utoipa;
     pub use utoipa::openapi::OpenApi;
-    
 }
 
 pub mod user_and_sesion_managment {
+
     use super::commons::*;
-    /// NOTE: routes sttarts from root.
-    /// NOTE: routes expected to correct nesting by api/mod.rs#init()
-    pub fn get_router() -> Router {todo!()}
-    pub fn get_openapi() -> OpenApi {todo!()}
 
-        
+    #[utoipa::path(
+        post,
+        path="/user/login", 
+        request_body(content=models::dtos::UserAuthReq)
 
+    )]
+    pub async fn login(
+        State(st): State<AppState>,
+        rq: Json<models::dtos::UserAuthReq>,
+    ) -> impl IntoResponse {
+        StatusCode::NOT_IMPLEMENTED
+    }
+    // Asks db to delete session by id.
+    pub async fn unlogin(State(st): State<AppState>, c: CookieJar) -> StatusCode {
+        StatusCode::NOT_IMPLEMENTED
+    }
+}
+pub mod admin_handlers {
+    use super::commons::*;
 
+    // Only admin shall register users.    // The first admin must be registered manually in DBMS
+    // by a sysop or via the setup script.
+    // TODO: consider adding a root user into migration scripts.
+    #[utoipa::path(post,
+            path="usermngr/register",
+            request_body=models::dtos::UserRegisterReq)]
+    pub fn register(State(st): State<AppState>, 
+    rq: Json<models::dtos::UserRegisterReq>) -> StatusCode {
+        StatusCode::NOT_IMPLEMENTED
+    }
 }
-pub mod admin {
-    use axum::routing::{get, post, put};
-    use axum::Json;
+pub mod user_handlers {
+    use super::commons::*;
 }
-pub mod user_managment {
-    use axum::routing::{get, post, put};
-    use axum::Json;
+pub mod ktest_solver_handlers {
+    use super::commons::*;
+    pub async fn list_test(
+        State(st): State<AppState>, c: CookieJar,
+        Query(rq): Query<Option<i64>> // Pass if asking for self.
+    ) -> Result<Json<models::dtos::TestListResponse>, StatusCode> {
+        Err(StatusCode::NOT_IMPLEMENTED)
+    } 
+
+    /// # Side effects:
+    /// Begins test, creating record of it and
+    /// providing user data of it.
+    pub async fn begin_ktest(State(st): State<AppState>, c: CookieJar,
+     Query(rq): Query<i64>
+) -> Result<Json<models::knowledge_test::KTestOngoing>, StatusCode> {
+        Err(StatusCode::NOT_IMPLEMENTED)
+    }
+    pub async fn answer_to_test(State(st): State<AppState>, c: CookieJar, 
+    ) -> StatusCode {
+        StatusCode::NOT_IMPLEMENTED
+    }
+    pub async fn handler(State(st): State<AppState>, c: CookieJar
+    )   ->  impl IntoResponse {
+        StatusCode::NOT_IMPLEMENTED
+    }
 }
-pub mod ktest_managment {
-    use axum::routing::{get, post, put};
-    use axum::Json;
+pub mod ktest_manager_handlers {
+    use super::commons::*;
 }
