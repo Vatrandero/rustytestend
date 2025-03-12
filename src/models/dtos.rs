b729049580cc71d2e8dd934bb0f88d0b8e35c131
Vadim Palatov <Vatrandero@gmail.com>
2025-03-12 15:16:44 +0300

    models: revise

--- src/models/dtos.rs
+++ src/models/dtos.rs
@@ -1,31 +1,57 @@
+
 /// Contins structure used usuaaly by web api.
 /// slices and or wrappers around other models.
 /// Some used to generate obtain full object, some
 /// passes thhrough  from api to place it needed without modification.
-
-
-use serde::{Serialize, Deserialize};
+use utoipa::ToSchema;
 pub use crate::models::knowledge_test::*;
-pub use crate::models::users::*; 
+pub use crate::models::users::*;
+use serde::{Deserialize, Serialize};
 
-
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, ToSchema)]
 pub struct UserAuthReq {
     pub login: String,
     pub password: String,
 }
-#[derive(Deserialize)]
+#[derive(Deserialize, ToSchema)]
 pub struct UserRegisterReq {
     pub login: String,
     pub password: String,
     pub first_name: String,
     pub seocnd_name: Option<String>,
     pub last_name: Option<String>,
-    pub role_asigned: String, 
-    pub groups_asigned: Option<Vec<String>>
+    pub role_asigned: String,
+    pub groups_asigned: Option<Vec<String>>,
+}
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct UserModReq{
+    pub login: Option<String>,
+    pub password: Option<String>,
+    pub first_name: Option<String>,
+    pub seocnd_name: Option<String>,
+    pub last_name: Option<String>,
+    pub role_asigned: Option<String>,
+    pub groups_asigned: Option<Vec<String>>,
 }
 
+/*  Maybe this one should be used for admin only in admin forms?
+Axim gives ability to return  Json<Vec<T>>, but when 
+admin is requester - we may want to return 
+something better formed.  */
+#[derive(Serialize, ToSchema)]
+pub struct TestListResponse {
+    user_id: i64,
+    asigned: Vec<AsignedTestResponse>
 
-pub struct TestListREsponse{ 
-    
+}
+// builds from test and asignment.
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct AsignedTestResponse { 
+    pub id: i64,  
+    pub name: String,
+    pub description: String, 
+    pub duration: i64,
+    pub attempts_avalable: i64,
+    pub open_from: i64, 
+    pub close_after: i64 
 }
--- src/models/knowledge_test/mod.rs
+++ src/models/knowledge_test/mod.rs
@@ -1,22 +1,33 @@
 pub mod report;
 
 use serde::{Deserialize, Serialize};
-#[derive(Hash, Serialize, Deserialize)]
-pub struct Question {
+use utoipa::ToSchema;
+/// internal use only
+#[derive(Hash)]
+pub struct QuestionPriv {
     pub id: u64,
-    pub title: String,
     pub question_body: String,
-    pub answers: Vec<Answers>,
+    pub answers: Vec<AnswersPriv>,
+
 }
 
-impl Question {
-    pub fn new(id: u64, title: String, question_body: String, answers: Vec<Answers>) -> Self {
-        Self { id, title, question_body, answers }
+impl QuestionPriv {
+    pub fn new(id: u64,  question_body: String, answers: Vec<AnswersPriv>) -> Self {
+        Self { id,  question_body, answers }
     }
 
 }
-#[derive(Hash, Serialize, Deserialize)]
-enum Answers {
+#[derive(Serialize, Deserialize, ToSchema)]
+pub enum Ansers{
+    Closed{
+        avalable: Vec<String>,
+        selected: usize // index, started from 0
+    }, // Avalable, selected,
+    Open(String)
+}
+/// For internal only use.
+#[derive(Hash )]
+pub enum AnswersPriv {
     Closed(
         Vec<String>, // contains answers wich may be selected by user..
         Vec<usize>,  // contain: Answers, indexes of rigt answer
@@ -24,7 +35,7 @@ enum Answers {
     Open, // answers did not stored as test part, operator need to check answer manualy.
 }
 
-impl Answers {
+impl AnswersPriv {
  fn is_close_valid(&self) {
     // TODO: For close answers - check if correct answers coresponds to lenght of answerss vec.
     todo!()
@@ -32,35 +43,41 @@ impl Answers {
 }
 /// This structure describes a ready-to-go test kit.
 /// When new session runs - it creates on this struct instance.
-#[derive(Serialize)]
 pub struct KnolewdgeTest {
     pub id: u64,
     pub title: String,
     pub description: Option<String>,
     pub max_duration_seconds: i64, // TODO:  consider change to chrono::Datetime
     pub minimum_pass_score: u8,
-    pub questions: Vec<Question>,
+    pub questions: Vec<QuestionPriv>,
 }
 
 // NOTE: Too short life, prototype.
-pub struct ktAsigment {
+pub struct KtAsigment {
     pub test_id: i64, 
     pub user_id: i64,
     pub open_from_timestamp: i64, 
     pub close_after_time_stamp: i64 
 }
-#[derive(Serialize)]
-pub struct KTestOngoing {
+
+pub struct KTestOngoingPriv {
     sesion_id: u64,
-    test: KnolewdgeTest, // TODO: Consider using Rc<>
-    ansered_questions: Vec<(Question, String)>,
+    test: KnolewdgeTest,    // TODO: Consider using Rc<>.
+                            // Considered: Realize after later as cache.
+    ansered_questions: Vec<(QuestionPriv, String)>,
     user_id: u64,
     session_start_time: i64,
 }
-#[derive(Serialize)]
-pub struct Ktestresult {
+pub struct KTestOngoing { 
+    session_id: i64,
+    test_id: i64,
+    
+
+
+}
+
+pub struct KtestResult {
     test: KnolewdgeTest,
     kt_session_started: i64,
     kt_session_ended: i64,
-    score: usize,
 }
